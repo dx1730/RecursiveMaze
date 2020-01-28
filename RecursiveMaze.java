40d01270a5151aeb89596e149f0252150fd5fab4
dx1730 <davidx94566@gmail.com>
2020-01-28 09:24:13 -0800

    printMaze method
    Created a printMaze method and made slight updates to canSolveMaze

--- RecursiveMaze.java
+++ RecursiveMaze.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class RecursiveMaze {
 
     public static void main() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("maze1.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("maze4.txt"));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int row = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
@@ -36,15 +36,15 @@ public class RecursiveMaze {
         if (row < 0 || col < 0 || row >= maze.length || col >= maze[0].length) {
             System.out.println("out of bounds");
             return false;
-        } else if (row == maze.length - 1 && col == maze[0].length - 1) {
-            System.out.println("solved");
-            return true;
         } else if (maze[row][col] == 1) {
             System.out.println("blocked");
             return false;
         } else if (maze[row][col] == 2) {
             System.out.println("already");
             return false;
+        } else if (row == maze.length - 1 && col == maze[0].length - 1) {
+            System.out.println("solved");
+            return true;
         } else {
             maze[row][col] = 2;
             return canSolveMaze(row + 1, col, maze) || canSolveMaze(row - 1, col, maze) ||
@@ -53,7 +53,20 @@ public class RecursiveMaze {
     }
 
     public static void printMaze(int[][] maze) {
-
+        for (int i = 0; i < maze.length; i++) {
+            printRepeat("-", maze[0].length * 2 + 1);
+            System.out.print("\n");
+            for (int j = 0; j < maze[0].length; j++) {
+                System.out.print("|");
+                if (maze[i][j] == 1) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("|");
+        }
+        printRepeat("-", maze[0].length * 2 + 1);
     }
 
     public static void printRepeat(String str, int n) {
